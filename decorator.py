8bbf09999100cb3d6e7bb7c8a3fca4da012e91ba
naumpopov18-art <naum.popov@mail.ru>
2026-01-27 14:41:07 +0500

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -129,3 +129,36 @@ def test_2():
 
 if __name__ == '__main__':
     test_2()
+
+
+#3 задание
+
+
+@logger('flat_generator.log')
+def flat_generator(list_of_lists):
+    for lists in list_of_lists:
+        for i in lists:
+            print(i)
+            yield i
+
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
+
